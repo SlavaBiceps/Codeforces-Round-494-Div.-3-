0abf206524c12040421e90da55e0936280be0216
SlavaBiceps <slava_ivanov_cs_12321@mail.ru>
2018-07-04 10:32:04 +0500

    OK

---
+++ A.pas
@@ -0,0 +1,19 @@
+var m:array of integer;
+n,i,max,sum:integer;
+begin
+readln(n);
+setlength(m,n);
+for i:=0 to n-1 do
+  read(m[i]);
+sort(m);
+max:=0;
+sum:=1;
+for i:=1 to n-1 do
+  if m[i]=m[i-1] then sum+=1
+    else
+      begin
+      if max<sum then max:=sum;
+      sum:=1;
+      end;
+writeln(max);
+end.
